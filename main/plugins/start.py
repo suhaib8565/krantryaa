5756f33eeb55086cc82be2ba464f4269eb85a0eb
Me <101977997+copycatb@users.noreply.github.com>
2022-04-21 10:59:11 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,107 +1,127 @@
-#  This file is part of the VIDEOconvertor distribution.
-#  Copyright (c) 2021 vasusen-code ; All rights reserved. 
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, version 3.
-#
-#  This program is distributed in the hope that it will be useful, but
-#  WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#  General Public License for more details.
-#
-#  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
+from .. import Drone, ACCESS_CHANNEL
 
-from .. import Drone, ACCESS_CHANNEL, AUTH_USERS
 from telethon import events, Button
+
 from LOCAL.localisation import START_TEXT as st
+
+from LOCAL.localisation import JPG as file
+
+from LOCAL.localisation import JPG4
+
 from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_text, SUPPORT_LINK
+
 from ethon.teleutils import mention
-from ethon.mystarts import vc_menu
-from main.plugins.actions import set_thumbnail, rem_thumbnail, heroku_restart
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/start"))
+
 async def start(event):
-    await event.reply(f'{st}', 
+
+    await event.reply(f'{st}', file=file,
+
                       buttons=[
+
                               [Button.inline("Menu.", data="menu")]
+
                               ])
+
     tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
+
     await Drone.send_message(int(ACCESS_CHANNEL), f'{tag} started the BOT')
+
     
+
 @Drone.on(events.callbackquery.CallbackQuery(data="menu"))
+
 async def menu(event):
-    await vc_menu(event)
+
+    await event.edit("**📑MENU.**",
+
+                    buttons=[[
+
+                         Button.inline("info.", data="info"),
+
+                         Button.inline("NOTICE", data="notice")],
+
+                         [
+
+                         Button.inline("SOURCE", data="source"),
+
+                         Button.inline("Help.", data="help")],
+
+                         [
+
+                         Button.url("DEVELOPER", url=f"{DEV}")]])
+
     
+
 @Drone.on(events.callbackquery.CallbackQuery(data="info"))
+
 async def info(event):
+
     await event.edit(f'**ℹ️NFO:**\n\n{info_text}',
+
                     buttons=[[
+
                          Button.inline("Menu.", data="menu")]])
+
     
+
 @Drone.on(events.callbackquery.CallbackQuery(data="notice"))
+
 async def notice(event):
+
     await event.answer(f'{spam_notice}', alert=True)
+
     
+
 @Drone.on(events.callbackquery.CallbackQuery(data="source"))
+
 async def source(event):
-    await event.edit(source_text,
+
+    await Drone.send_message(event.chat_id, source_text,
+
                     buttons=[[
-                         Button.url("on youtube", url="https://www.youtube.com/channel/UCsNkiEm2go3Zyzum7e3PpBg/videos"),
-                         Button.url("on telegram ", url="https://t.me/Compressed_lectures")]])
-                         
-                    
+
+                         Button.url("youtube.", url="https://youtube.com/channel/UCsNkiEm2go3Zyzum7e3PpBg")]])
+
+                                    
+
+    
+
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
+
 async def help(event):
-    await event.edit('**👥HELP & SETTINGS**',
+
+    await event.edit('**👥HELP.**',
+
                     buttons=[[
-                         Button.inline("SET THUMB", data="sett"),
-                         Button.inline("REM THUMB", data='remt')],
+
+                         Button.inline("PLUGINS.", data="plugins"),
+
+                         Button.url("SUPPORT.", url=f"{SUPPORT_LINK}")],
+
                          [
-                         Button.inline("PLUGINS", data="plugins"),
-                         Button.inline("RESTART", data="restart")],
-                         [Button.url("SUPPORT", url=f"{SUPPORT_LINK}")],
-                         [
-                         Button.inline("BACK", data="menu")]])
+
+                         Button.inline("Menu.", data="menu")]])
+
     
+
 @Drone.on(events.callbackquery.CallbackQuery(data="plugins"))
+
 async def plugins(event):
+
     await event.edit(f'{help_text}',
-                    buttons=[[Button.inline("Menu.", data="menu")]])
-                   
- #-----------------------------------------------------------------------------------------------                            
+
+                    buttons=[[
+
+                         Button.inline("Menu.", data="menu")]])
+
     
-@Drone.on(events.callbackquery.CallbackQuery(data="sett"))
-async def sett(event):    
-    button = await event.get_message()
-    msg = await button.get_reply_message() 
-    await event.delete()
-    async with Drone.conversation(event.chat_id) as conv: 
-        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
-        x = await conv.get_reply()
-        if not x.media:
-            xx.edit("No media found.")
-        mime = x.file.mime_type
-        if not 'png' in mime:
-            if not 'jpg' in mime:
-                if not 'jpeg' in mime:
-                    return await xx.edit("No image found.")
-        await set_thumbnail(event, x.media)
-        await xx.delete()
-        
-@Drone.on(events.callbackquery.CallbackQuery(data="remt"))
-async def remt(event):  
-    await event.delete()
-    await rem_thumbnail(event)
+
     
-@Drone.on(events.callbackquery.CallbackQuery(data="restart"))
-async def res(event):
-    if not f'{event.sender_id}' == f'{int(AUTH_USERS)}':
-        return await event.edit("Only authorized user can restart!")
-    result = await heroku_restart()
-    if result is None:
-        await event.edit("You have not filled `HEROKU_API` and `HEROKU_APP_NAME` vars.")
-    elif result is False:
-        await event.edit("An error occured!")
-    elif result is True:
-        await event.edit("Restarting app, wait for a minute.")
+
+    
+
+    
+
+    
